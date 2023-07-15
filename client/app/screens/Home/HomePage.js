00004b9fb30878ddfa5c7eb4e53923fb1d1ac2d0
maria <Mariapanov18@gmail.com>
2023-07-16 01:57:57 +0300

    15072023

--- client/app/screens/Home/HomePage.js
+++ client/app/screens/Home/HomePage.js
@@ -1,7 +1,5 @@
-//
-
 import React, { useState, useEffect } from 'react';
-import { ScrollView, View, ImageBackground, Animated, Text } from 'react-native';
+import { ScrollView, Text, View, ImageBackground, Animated } from 'react-native';
 import Planner from './PlannerScreen/Planner';
 import { LinearGradient } from 'expo-linear-gradient';
 import HomeContext from './HomeContext';
@@ -17,42 +15,65 @@ const HomeScreen = ({ navigation }) => {
     setContent(newContent);
   };
 
-  useEffect(() => {
-    const imageWidth = 1000; // The width of the image
-    const lineDuration = 15000; // Duration in milliseconds for line animation
-
-    Animated.timing(lineAnimation, {
-      toValue: imageWidth,
-      duration: lineDuration,
-      useNativeDriver: false,
-    }).start();
-  }, []);
-
   const image = {
     uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz4-KQxdWIWDFVosDliJ-HeqkLUy8_65MhJVTK2z2QMdhPF04MY_7opdD4VHco49B7AwI&usqp=CAU',
   };
 
+  useEffect(() => {
+    const imageWidth = 1000;
+    const lineDuration = 50000;
+
+    const startAnimationLoop = () => {
+      const lineMoveAnimation = Animated.timing(lineAnimation, {
+        toValue: imageWidth,
+        duration: lineDuration,
+        useNativeDriver: false,
+      });
+      const lineResetAnimation = Animated.timing(lineAnimation, {
+        toValue: -100,
+        duration: 0,
+        useNativeDriver: false,
+      });
+      const lineLoopAnimation = Animated.sequence([lineMoveAnimation, lineResetAnimation]); // Infinite scroll effect
+
+      const animationLoop = Animated.loop(lineLoopAnimation); // Start the infinite loop animation
+
+      animationLoop.start();
+    };
+
+    startAnimationLoop();
+    return () => {
+      lineAnimation.setValue(-100);
+    };
+  }, []);
+
+  const lineAnimation = new Animated.Value(-190);
+
   const AnimatedLineWithText = ({ width, text }) => (
-    <View style={{}}>
-      <Animated.View
-        style={[
-          {
-            right: width,
-          },
-        ]}
-      />
-      <Animated.View
-        style={[
-          {
-            left: width, // Set the left position of the text container to match the animated line width
-          },
-        ]}>
-        <Text style={{}}>{text}</Text>
+    <View
+      style={{
+        justifyContent: 'center',
+        alignContent: 'center',
+      }}>
+      <Animated.View style={[{ right: width }]} />
+      <Animated.View style={[{ left: width }]}>
+        <Text
+          style={{
+            color: '#907AD6',
+            fontSize: 20,
+            fontWeight: '500',
+            textShadowColor: '#000',
+            textShadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            textShadowRadius: 3,
+          }}>
+          {text}
+        </Text>
       </Animated.View>
     </View>
   );
-
-  const lineAnimation = new Animated.Value(-100);
 
   return (
     <HomeContext.Provider value={{ switchContent: switchContent }}>
@@ -63,7 +84,7 @@ const HomeScreen = ({ navigation }) => {
             resizeMode='cover'
             style={homeStyle.imgBackgroudContainer}>
             <ScreenHeading />
-            <AnimatedLineWithText width={lineAnimation} text='Welcome to your account' />
+            <AnimatedLineWithText width={lineAnimation} text='LearnMe welcomes you!' />
           </ImageBackground>
         </View>
         <ScrollView style={{ height: '85%' }}>{content}</ScrollView>
