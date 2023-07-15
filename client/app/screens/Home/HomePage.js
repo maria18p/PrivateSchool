c6a37e9ba279edba70e6a15eb35e65c7fc4f37e5
maria <Mariapanov18@gmail.com>
2023-07-16 00:44:50 +0300

    15072023

--- client/app/screens/Home/HomePage.js
+++ client/app/screens/Home/HomePage.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { ScrollView, View, ImageBackground } from 'react-native';
+//
+
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, ImageBackground, Animated, Text } from 'react-native';
 import Planner from './PlannerScreen/Planner';
 import { LinearGradient } from 'expo-linear-gradient';
 import HomeContext from './HomeContext';
@@ -15,9 +17,42 @@ const HomeScreen = ({ navigation }) => {
     setContent(newContent);
   };
 
+  useEffect(() => {
+    const imageWidth = 1000; // The width of the image
+    const lineDuration = 15000; // Duration in milliseconds for line animation
+
+    Animated.timing(lineAnimation, {
+      toValue: imageWidth,
+      duration: lineDuration,
+      useNativeDriver: false,
+    }).start();
+  }, []);
+
   const image = {
     uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz4-KQxdWIWDFVosDliJ-HeqkLUy8_65MhJVTK2z2QMdhPF04MY_7opdD4VHco49B7AwI&usqp=CAU',
   };
+
+  const AnimatedLineWithText = ({ width, text }) => (
+    <View style={{}}>
+      <Animated.View
+        style={[
+          {
+            right: width,
+          },
+        ]}
+      />
+      <Animated.View
+        style={[
+          {
+            left: width, // Set the left position of the text container to match the animated line width
+          },
+        ]}>
+        <Text style={{}}>{text}</Text>
+      </Animated.View>
+    </View>
+  );
+
+  const lineAnimation = new Animated.Value(-100);
 
   return (
     <HomeContext.Provider value={{ switchContent: switchContent }}>
@@ -28,6 +63,7 @@ const HomeScreen = ({ navigation }) => {
             resizeMode='cover'
             style={homeStyle.imgBackgroudContainer}>
             <ScreenHeading />
+            <AnimatedLineWithText width={lineAnimation} text='Welcome to your account' />
           </ImageBackground>
         </View>
         <ScrollView style={{ height: '85%' }}>{content}</ScrollView>
