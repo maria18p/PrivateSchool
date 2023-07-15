37525bacbd069689517320b58838f28b6caa0796
maria <Mariapanov18@gmail.com>
2023-07-16 02:13:03 +0300

    15072023

--- client/app/screens/Home/HomePage.js
+++ client/app/screens/Home/HomePage.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { ScrollView, Text, View, ImageBackground, Animated } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, ImageBackground } from 'react-native';
 import Planner from './PlannerScreen/Planner';
 import { LinearGradient } from 'expo-linear-gradient';
 import HomeContext from './HomeContext';
@@ -19,62 +19,6 @@ const HomeScreen = ({ navigation }) => {
     uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz4-KQxdWIWDFVosDliJ-HeqkLUy8_65MhJVTK2z2QMdhPF04MY_7opdD4VHco49B7AwI&usqp=CAU',
   };
 
-  useEffect(() => {
-    const imageWidth = 1000;
-    const lineDuration = 50000;
-
-    const startAnimationLoop = () => {
-      const lineMoveAnimation = Animated.timing(lineAnimation, {
-        toValue: imageWidth,
-        duration: lineDuration,
-        useNativeDriver: false,
-      });
-      const lineResetAnimation = Animated.timing(lineAnimation, {
-        toValue: -100,
-        duration: 0,
-        useNativeDriver: false,
-      });
-      const lineLoopAnimation = Animated.sequence([lineMoveAnimation, lineResetAnimation]); // Infinite scroll effect
-
-      const animationLoop = Animated.loop(lineLoopAnimation); // Start the infinite loop animation
-
-      animationLoop.start();
-    };
-
-    startAnimationLoop();
-    return () => {
-      lineAnimation.setValue(-100);
-    };
-  }, []);
-
-  const lineAnimation = new Animated.Value(-190);
-
-  const AnimatedLineWithText = ({ width, text }) => (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignContent: 'center',
-      }}>
-      <Animated.View style={[{ right: width }]} />
-      <Animated.View style={[{ left: width }]}>
-        <Text
-          style={{
-            color: '#907AD6',
-            fontSize: 20,
-            fontWeight: '500',
-            textShadowColor: '#000',
-            textShadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            textShadowRadius: 3,
-          }}>
-          {text}
-        </Text>
-      </Animated.View>
-    </View>
-  );
-
   return (
     <HomeContext.Provider value={{ switchContent: switchContent }}>
       <LinearGradient colors={['#F2F4FF', '#96C5F7', '#454ADE']} style={styles.container}>
@@ -84,7 +28,6 @@ const HomeScreen = ({ navigation }) => {
             resizeMode='cover'
             style={homeStyle.imgBackgroudContainer}>
             <ScreenHeading />
-            <AnimatedLineWithText width={lineAnimation} text='LearnMe welcomes you!' />
           </ImageBackground>
         </View>
         <ScrollView style={{ height: '85%' }}>{content}</ScrollView>
