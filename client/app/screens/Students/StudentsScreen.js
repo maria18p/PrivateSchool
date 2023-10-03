e906a447e4fd5358a79ec9c82f2e69eafec6b0dd
maria <Mariapanov18@gmail.com>
2023-10-04 01:06:24 +0300

    04102023

--- client/app/screens/Students/StudentsScreen.js
+++ client/app/screens/Students/StudentsScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, ScrollView, Alert, ImageBackground } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import { getAllUsers, updateStudentActive, updateStudentInactive } from '../../api/User_requests';
@@ -6,14 +6,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getTeacherStudents } from '../../api/Pairing_requests';
 import styles from '../../styles/StudentsStyle';
 import { LinearGradient } from 'expo-linear-gradient';
-import Popover from 'react-native-popover-view';
 
 const Students = () => {
    const backgroundImage = require('../../assets/bg/students_bg.jpeg');
    const dispatch = useDispatch();
    const userData = useSelector((state) => state.user);
 
-   const popoverRef = useRef(null);
    const [myStudents, setMyStudents] = useState(null);
    const [allStudents, setAllStudents] = useState(null);
    const [mode, setMode] = useState(null);
@@ -67,28 +65,6 @@ const Students = () => {
       setOpenPopoverKey(updatedPopoverKeys);
    };
 
-   const ShowPopover = ({ student, popoverKey }) => {
-      const isStudentPopoverVisible = openPopoverKey[popoverKey] !== null;
-      return (
-         <Popover
-            ref={popoverRef}
-            isVisible={isStudentPopoverVisible}
-            popoverStyle={{
-               padding: 10,
-               backgroundColor: 'white',
-               borderRadius: 5,
-               shadowColor: '#000000',
-               shadowOffset: { width: 0, height: 2 },
-               shadowOpacity: 0.3,
-               shadowRadius: 4,
-            }}>
-            <TouchableOpacity onPress={() => handlePopoverClose(popoverKey)}>
-               <Text>{student.email}</Text>
-            </TouchableOpacity>
-         </Popover>
-      );
-   };
-
    const studentData = () => {
       if (!mode || !Array.isArray(mode)) {
          return <></>;
@@ -121,7 +97,6 @@ const Students = () => {
                            onMouseLeave={() => handlePopoverClose(key)}>
                            <Text style={styles.cellTxt}>{student.email}</Text>
                         </TouchableOpacity>{' '}
-                        <ShowPopover student={student} key={key} />
                      </DataTable.Cell>
                      <DataTable.Cell numeric textStyle={styles.cellTxt} style={styles.status}>
                         {student.isActive ? 'Active' : 'Not Active'}
@@ -223,9 +198,9 @@ const Students = () => {
          <ScrollView>
             <DataTable>
                <DataTable.Header style={styles.tblHeader}>
-                  <DataTable.Title textStyle={{ fontSize: 14 }}>Full name</DataTable.Title>
+                  <DataTable.Title textStyle={{ fontSize: 14 }}>Name</DataTable.Title>
                   <DataTable.Title textStyle={{ fontSize: 14 }}>Email</DataTable.Title>
-                  <DataTable.Title textStyle={{ fontSize: 14, marginLeft: -30 }}>
+                  <DataTable.Title textStyle={{ fontSize: 14, marginLeft: -36 }}>
                      Status
                   </DataTable.Title>
                </DataTable.Header>
